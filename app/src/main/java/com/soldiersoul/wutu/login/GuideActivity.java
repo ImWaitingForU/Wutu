7cc65a19f4be90ed96e69c10d725a4261fb640f8
ImWaitingForU <870536460@qq.com>
2016-12-27 21:51:01 +0800

    add引导页

---
+++ app/src/main/java/com/soldiersoul/wutu/login/GuideActivity.java
@@ -0,0 +1,21 @@
+package com.soldiersoul.wutu.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.soldiersoul.wutu.R;
+
+/**
+ * @author Chan
+ * 导航页
+ */
+public class GuideActivity extends AppCompatActivity {
+
+    private
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_guide_activitiy);
+    }
+}
