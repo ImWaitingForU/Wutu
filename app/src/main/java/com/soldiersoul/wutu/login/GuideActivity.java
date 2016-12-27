c1428687932d0e59ff7f1dca4476ec93268c64e8
ImWaitingForU <870536460@qq.com>
2016-12-27 23:44:50 +0800

    编辑登录界面，添加了一些工具，添加BaseActivity

--- app/src/main/java/com/soldiersoul/wutu/login/GuideActivity.java
+++ app/src/main/java/com/soldiersoul/wutu/login/GuideActivity.java
@@ -5,17 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.soldiersoul.wutu.R;
 
+import cn.bingoogolapple.bgabanner.BGABanner;
+
 /**
  * @author Chan
  * 导航页
  */
 public class GuideActivity extends AppCompatActivity {
 
-    private
+    private BGABanner mBanner;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_guide_activitiy);
+
+        mBanner = (BGABanner) findViewById (R.id.banner);
+        //TODO 设置banner轮播图片
     }
 }
--- app/src/main/java/com/soldiersoul/wutu/login/LoginActivity.java
+++ app/src/main/java/com/soldiersoul/wutu/login/LoginActivity.java
@@ -1,9 +1,38 @@
 package com.soldiersoul.wutu.login;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.maksim88.passwordedittext.PasswordEditText;
+import com.soldiersoul.wutu.R;
+import com.soldiersoul.wutu.utils.BaseActivity;
+
+import butterknife.BindView;
 
 
-public class LoginActivity extends AppCompatActivity{
+public class LoginActivity extends BaseActivity {
 
-  
+    @BindView (R.id.et_userName) EditText etPhone;
+    @BindView (R.id.et_pwd)  PasswordEditText etPwd;
+    @BindView (R.id.btn_login) Button btnLogin;
+
+    @Override
+    protected void onCreate (@Nullable Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        initHandler (new Handler.Callback () {
+            @Override
+            public boolean handleMessage (Message msg) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public int getContentViewId () {
+        return R.layout.activity_login;
+    }
 }
