b757fbda55a7045f777f99738eb593e25f6602fa
ImWaitingForU <870536460@qq.com>
2017-01-04 21:30:53 +0800

    主界面使用四个Fragment，宿主Activity为MainActivity

--- app/src/main/java/com/soldiersoul/wutu/home/MainActivity.java
+++ app/src/main/java/com/soldiersoul/wutu/home/MainActivity.java
@@ -1,15 +1,19 @@
 package com.soldiersoul.wutu.home;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.soldiersoul.wutu.R;
+import com.soldiersoul.wutu.utils.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
-        setContentView (R.layout.activity_main);
+    }
+
+    @Override
+    public int getContentViewId () {
+        return R.layout.activity_main;
     }
 }
