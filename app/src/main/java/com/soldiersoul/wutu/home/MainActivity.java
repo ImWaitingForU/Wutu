48fbc33f73fa90fc11ec44e9786ca3d6dba5c865
ImWaitingForU <870536460@qq.com>
2017-01-07 18:31:46 +0800

    fragment使用toolbar

--- app/src/main/java/com/soldiersoul/wutu/home/MainActivity.java
+++ app/src/main/java/com/soldiersoul/wutu/home/MainActivity.java
@@ -2,8 +2,13 @@ package com.soldiersoul.wutu.home;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.soldiersoul.wutu.R;
+import com.soldiersoul.wutu.manage.ManageFragment;
+import com.soldiersoul.wutu.me.MeFragment;
+import com.soldiersoul.wutu.military.MilitaryFragment;
+import com.soldiersoul.wutu.society.SocietyFragment;
 import com.soldiersoul.wutu.utils.BaseActivity;
 import com.soldiersoul.wutu.views.BottomBar;
 
@@ -14,6 +19,10 @@ public class MainActivity extends BaseActivity implements BottomBar.BottomBarCli
     @BindView (R.id.bottomBar) BottomBar mBottomBar;
 
     private FragmentManager mFragmentManager;
+    private MilitaryFragment mMilitaryFragment;
+    private SocietyFragment mSocietyFragment;
+    private ManageFragment mManageFragment;
+    private MeFragment mMeFragment;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -25,6 +34,77 @@ public class MainActivity extends BaseActivity implements BottomBar.BottomBarCli
 
     private void initFragments () {
 
+        mBottomBar.setBottomBarClickedListener (this);
+        //初始化时添加军事天地fragment
+        showMilitaryFragments ();
+    }
+
+    private void showMilitaryFragments () {
+        FragmentTransaction transaction = mFragmentManager.beginTransaction ();
+        if (mMilitaryFragment == null) {
+            mMilitaryFragment = new MilitaryFragment ();
+            transaction.add (R.id.main_layout, mMilitaryFragment);
+        }
+        hideAllFragment ();
+        transaction.show (mMilitaryFragment);
+        transaction.commitAllowingStateLoss ();
+    }
+
+    private void showSocietyFragments () {
+        FragmentTransaction transaction = mFragmentManager.beginTransaction ();
+        if (mSocietyFragment == null) {
+            mSocietyFragment = new SocietyFragment ();
+            transaction.add (R.id.main_layout, mSocietyFragment);
+        }
+        hideAllFragment ();
+        transaction.show (mSocietyFragment);
+        transaction.commitAllowingStateLoss ();
+    }
+
+    private void showManageFragments () {
+        FragmentTransaction transaction = mFragmentManager.beginTransaction ();
+        if (mManageFragment == null) {
+            mManageFragment = new ManageFragment ();
+            transaction.add (R.id.main_layout, mManageFragment);
+        }
+        hideAllFragment ();
+        transaction.show (mManageFragment);
+        transaction.commitAllowingStateLoss ();
+    }
+
+    private void showMeFragments () {
+        FragmentTransaction transaction = mFragmentManager.beginTransaction ();
+        if (mMeFragment == null) {
+            mMeFragment = new MeFragment ();
+            transaction.add (R.id.main_layout, mMeFragment);
+        }
+        hideAllFragment ();
+        transaction.show (mMeFragment);
+        transaction.commitAllowingStateLoss ();
+    }
+
+    /**
+     * 隐藏所有Fragment
+     */
+    private void hideAllFragment () {
+        FragmentTransaction transaction = mFragmentManager.beginTransaction ();
+        if (mMilitaryFragment != null && mMilitaryFragment.isVisible ()) {
+            transaction.hide (mMilitaryFragment);
+        }
+
+        if (mSocietyFragment != null && mSocietyFragment.isVisible ()) {
+            transaction.hide (mSocietyFragment);
+        }
+
+        if (mManageFragment != null && mManageFragment.isVisible ()) {
+            transaction.hide (mManageFragment);
+        }
+
+        if (mMeFragment != null && mMeFragment.isVisible ()) {
+            transaction.hide (mMeFragment);
+        }
+
+        transaction.commitAllowingStateLoss ();
     }
 
     @Override
@@ -34,21 +114,21 @@ public class MainActivity extends BaseActivity implements BottomBar.BottomBarCli
 
     @Override
     public void onBottomBar1Clicked () {
-
+        showMilitaryFragments ();
     }
 
     @Override
     public void onBottomBar2Clicked () {
-
+        showSocietyFragments ();
     }
 
     @Override
     public void onBottomBar3Clicked () {
-
+        showManageFragments ();
     }
 
     @Override
     public void onBottomBar4Clicked () {
-
+        showMeFragments ();
     }
 }
