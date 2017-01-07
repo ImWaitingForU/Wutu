49492fddf6e34bdbd2ac98dcead2b8145a43492b
wxj1018 <605074293@qq.com>
2017-01-07 10:38:59 +0800

    再同步
    再同步

--- app/src/main/java/com/soldiersoul/wutu/military/MilitaryWorldActivity.java
+++ app/src/main/java/com/soldiersoul/wutu/military/MilitaryWorldActivity.java
@@ -1,17 +1,20 @@
 package com.soldiersoul.wutu.military;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
 
 import com.soldiersoul.wutu.R;
+import com.soldiersoul.wutu.utils.BaseActivity;
 
-public class MilitaryWorldActivity extends Activity {
+public class MilitaryWorldActivity extends BaseActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_military_world);
+
+    }
+
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_military_world;
     }
 }
