393e01195159c689ea2221d2c2322f10f339d5fc
ImWaitingForU <870536460@qq.com>
2017-01-09 23:53:15 +0800

    实现社团界面首页初步

--- app/src/main/java/com/soldiersoul/wutu/society/SocietyFragment.java
+++ app/src/main/java/com/soldiersoul/wutu/society/SocietyFragment.java
@@ -4,17 +4,13 @@ package com.soldiersoul.wutu.society;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.soldiersoul.wutu.R;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * 社团Fragment
@@ -23,15 +19,6 @@ public class SocietyFragment extends Fragment {
 
     private static final String TAG = "SocietyFragment";
 
-    @BindView (R.id.rvSocietyList) RecyclerView rvSocietyList;
-
-    /**
-     * 添加新社团
-     */
-    @OnClick (R.id.ivAdd)
-    void addNewSociety () {
-        Log.d (TAG, "--addNewSociety--");
-    }
 
     public SocietyFragment () {
     }
