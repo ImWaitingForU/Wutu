d67a2bda33ac7fb790a1961edd675a5a18db9725
ImWaitingForU <870536460@qq.com>
2017-02-01 13:52:43 +0800

    add society staff

--- app/src/main/java/com/soldiersoul/wutu/society/SocietyFragment.java
+++ app/src/main/java/com/soldiersoul/wutu/society/SocietyFragment.java
@@ -4,12 +4,19 @@ package com.soldiersoul.wutu.society;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.flyco.tablayout.SlidingTabLayout;
 import com.soldiersoul.wutu.R;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -17,8 +24,34 @@ import butterknife.ButterKnife;
  */
 public class SocietyFragment extends Fragment {
 
-    private static final String TAG = "SocietyFragment";
+    @BindView (R.id.tl) SlidingTabLayout stl;
+    @BindView (R.id.vpSociety) ViewPager vpSociety;
 
+    private static final String TAG = "SocietyFragment";
+    private ArrayList<Fragment> mFragments = new ArrayList<> ();
+    private final String[] mTitles = {"社团信息", "社团照片", "社团活动", "社团积分"};
+    private MyPagerAdapter mAdapter;
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+        public MyPagerAdapter (FragmentManager fm) {
+            super (fm);
+        }
+
+        @Override
+        public int getCount () {
+            return mFragments.size ();
+        }
+
+        @Override
+        public CharSequence getPageTitle (int position) {
+            return mTitles[position];
+        }
+
+        @Override
+        public Fragment getItem (int position) {
+            return mFragments.get (position);
+        }
+    }
 
     public SocietyFragment () {
     }
@@ -27,6 +60,18 @@ public class SocietyFragment extends Fragment {
     public void onViewCreated (View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated (view, savedInstanceState);
         ButterKnife.bind (this, view);
+
+        //todo:初始化Fragment集合
+        if (mFragments != null && mFragments.isEmpty ()) {
+            mFragments.add (new SocietyBaseInfoFragment ());
+            mFragments.add (new SocietyPhotoFragment ());
+            mFragments.add (new SocietyActFragment ());
+            mFragments.add (new SocietyIntegralFragment ());
+        }
+
+        mAdapter = new MyPagerAdapter (getFragmentManager ());
+        vpSociety.setAdapter (mAdapter);
+        stl.setViewPager (vpSociety);
     }
 
     @Override
@@ -34,73 +79,73 @@ public class SocietyFragment extends Fragment {
         return inflater.inflate (R.layout.fragment_society, container, false);
     }
 
-//    /**
-//     * 跳转到定位界面
-//     */
-//    @OnClick (R.id.rlLocation)
-//    void gotoLocation () {
-//        startActivity (new Intent (getActivity (), LocationActivity.class));
-//    }
-//
-//    /**
-//     * 社团特色活动
-//     */
-//    @OnClick (R.id.tvActivity)
-//    void goToActivity () {
-//        startActivity (new Intent (getActivity (),SocietyActActivity.class));
-//    }
-//
-//    /**
-//     * 社团荣誉
-//     */
-//    @OnClick (R.id.tvHonor)
-//    void goToHonor () {
-//        startActivity (new Intent (getActivity (),HonorActivity.class));
-//    }
-//
-//    /**
-//     * 社团图片
-//     */
-//    @OnClick (R.id.tvPhoto)
-//    void goToPhoto () {
-//        startActivity (new Intent (getActivity (),PhotoActivity.class));
-//
-//    }
-//
-//    /**
-//     * 社团人员资料
-//     */
-//    @OnClick (R.id.tvMember)
-//    void goToMember () {
-//        startActivity (new Intent (getActivity (),MemberActivity.class));
-//    }
-//
-//    /**
-//     * 社团培训大厅
-//     */
-//    @OnClick (R.id.tvTrain)
-//    void goToTrain () {
-//        startActivity (new Intent (getActivity (),TrainActivity.class));
-//
-//    }
-//
-//    /**
-//     * 我的贡献
-//     */
-//    @OnClick (R.id.tvContribute)
-//    void goToContribute () {
-//        startActivity (new Intent (getActivity (),ContributeActivity.class));
-//
-//    }
-//
-//    /**
-//     * 积分兑换大厅
-//     */
-//    @OnClick (R.id.tvIntegral)
-//    void goToIntegral () {
-//        startActivity (new Intent (getActivity (),IntegralActivity.class));
-//
-//    }
+    //    /**
+    //     * 跳转到定位界面
+    //     */
+    //    @OnClick (R.id.rlLocation)
+    //    void gotoLocation () {
+    //        startActivity (new Intent (getActivity (), LocationActivity.class));
+    //    }
+    //
+    //    /**
+    //     * 社团特色活动
+    //     */
+    //    @OnClick (R.id.tvActivity)
+    //    void goToActivity () {
+    //        startActivity (new Intent (getActivity (),SocietyActActivity.class));
+    //    }
+    //
+    //    /**
+    //     * 社团荣誉
+    //     */
+    //    @OnClick (R.id.tvHonor)
+    //    void goToHonor () {
+    //        startActivity (new Intent (getActivity (),HonorActivity.class));
+    //    }
+    //
+    //    /**
+    //     * 社团图片
+    //     */
+    //    @OnClick (R.id.tvPhoto)
+    //    void goToPhoto () {
+    //        startActivity (new Intent (getActivity (),PhotoActivity.class));
+    //
+    //    }
+    //
+    //    /**
+    //     * 社团人员资料
+    //     */
+    //    @OnClick (R.id.tvMember)
+    //    void goToMember () {
+    //        startActivity (new Intent (getActivity (),MemberActivity.class));
+    //    }
+    //
+    //    /**
+    //     * 社团培训大厅
+    //     */
+    //    @OnClick (R.id.tvTrain)
+    //    void goToTrain () {
+    //        startActivity (new Intent (getActivity (),TrainActivity.class));
+    //
+    //    }
+    //
+    //    /**
+    //     * 我的贡献
+    //     */
+    //    @OnClick (R.id.tvContribute)
+    //    void goToContribute () {
+    //        startActivity (new Intent (getActivity (),ContributeActivity.class));
+    //
+    //    }
+    //
+    //    /**
+    //     * 积分兑换大厅
+    //     */
+    //    @OnClick (R.id.tvIntegral)
+    //    void goToIntegral () {
+    //        startActivity (new Intent (getActivity (),IntegralActivity.class));
+    //
+    //    }
 
 
 }
